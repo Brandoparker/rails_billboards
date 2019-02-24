9b0a4fafa64cc77e822496a8005a22dde1e8bd31
Brandon Parker <newboots@gmail.com>
2019-02-23 19:09:24 -0700

    artists and songs

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
     resources :songs
   end
   
+  resources :songs do
+    resources :artists
+  end  
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_23_183720) do
+ActiveRecord::Schema.define(version: 2019_02_23_213417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.string "genre"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "billboards", force: :cascade do |t|
     t.string "name"
